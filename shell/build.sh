89ebf00894495735f4be6aaf687067903b389e92
Nigel Christian <nigel.l.christian@gmail.com>
2020-12-28 13:25:01 -0500

    update build script for kernel work

--- shell/build.sh
+++ shell/build.sh
@@ -3,14 +3,17 @@
 # run from root of linux mainline or next after making changes
 # dont forget to checkpatch.pl -f <file> !
 # do git oneline to get formatting of patch subject
-echo did you update the config lately?
+echo grabbing latest changes...
+git checkout master && git stash && git pull && git branch -D work && git checkout -b work
+echo updating config...
+cp /boot/config-5.9.16-200.fc33.x86_64 .config
 sleep 5
 make listnewconfig
 lsmod > /tmp/my-lsmod
 make LSMOD=/tmp/my-lsmod localmodconfig
 echo compiling kernel
 sleep 5
-make -j3 all
+make -j4 all
 # get all dmesg to compare after install
 echo collecting dmesg
 sleep 5
@@ -23,10 +26,11 @@ dmesg -t -l err > dmesg_current_err
 dmesg -t -l warn > dmesg_current_warn
 echo installing kernel
 sleep 5
-sudo make modules_install install && reboot
+sudo make modules_install install
+echo restart to boot into modified kernel
 # if all good git commit -m "message" <file>
 # git format-patch -1 <commitId>
 # mutt -H 000-mychange.patch
 
 # On first compile there might issues 
-# install openssl-devel, elf-utils and try make prepare
+# install openssl-devel, dwarves, elf-utils and try make prepare
